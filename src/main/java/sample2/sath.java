f339d9fe0606dd44570ebd0762f45a7e11a438d5
Sathiya.mahalingam <Sathiya.mahalingam@GSR-Lap32.GSRCHENNAI.LOCAL>
2017-03-01 14:17:07 +0530

    branch3 commit

--- src/main/java/sample2/sath.java
+++ src/main/java/sample2/sath.java
@@ -1,13 +1,9 @@
 package sample2;
 
-public class Printword {
+public class sath {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("print word");
-		
-		
 
 	}
 
